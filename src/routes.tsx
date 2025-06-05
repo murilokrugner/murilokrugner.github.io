f971aa4266036b63bd28d45442f8903c68c6abb5
Murilo Krugner <murilokrugner@hotmail.com>
2025-06-05 16:52:49 -0300

    Update routes.tsx

--- src/routes.tsx
+++ src/routes.tsx
@@ -5,11 +5,13 @@ import {
   Link
 } from "react-router-dom";
 import Home from "./pages/Home";
+import config from "../public/config.json"
 
 export default function routes() {
   return (
     <Switch>
       <Route path="/" element={<Home/>} />
+      <Route path="/config.json" element={() => config}</Route>} />
     </Switch>
   )
 }
